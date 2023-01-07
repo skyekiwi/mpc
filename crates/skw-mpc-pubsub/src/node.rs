bcb613622ce075d49c7b670bb4f39f677f3cda67
Song Zhou <song.zhou@skye.kiwi>
2023-01-07 17:43:36 +0800

    replace gossipsub with floodsub

--- crates/skw-mpc-pubsub/src/node.rs
+++ crates/skw-mpc-pubsub/src/node.rs
@@ -1,17 +1,9 @@
-use libp2p::gossipsub::MessageId;
-use libp2p::gossipsub::{
-    Gossipsub, GossipsubEvent, GossipsubMessage, IdentTopic as Topic, MessageAuthenticity,
-    ValidationMode,
-};
+use libp2p::floodsub::Floodsub;
 use libp2p::swarm::keep_alive;
 use libp2p::{
-    gossipsub, identity, mdns, swarm::NetworkBehaviour, swarm::SwarmEvent, PeerId, Swarm,
+    floodsub, identity, mdns, swarm::NetworkBehaviour, swarm::SwarmEvent, PeerId, Swarm,
 };
 use tokio::sync::Notify;
-
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::time::Duration;
 
 use anyhow::{Result};
 use futures::{Sink, Stream, StreamExt};
@@ -19,9 +11,9 @@ use std::{cell::RefCell, rc::Rc};
 
 #[derive(NetworkBehaviour)]
 pub struct MpcPubsubBahavior {
-    pub gossipsub: Gossipsub,
-    pub mdns: mdns::async_io::Behaviour,
-    pub keep_alive: keep_alive::Behaviour,
+    floodsub: floodsub::Floodsub,
+    mdns: mdns::async_io::Behaviour,
+    keep_alive: keep_alive::Behaviour,
 }
 
 pub struct MpcPubsub {
@@ -38,28 +30,13 @@ impl MpcPubsub {
 
         let transport = libp2p::development_transport(local_key.clone()).await?;
 
-        // Placeholder
-        let message_id_fn = |message: &GossipsubMessage| {
-            let mut s = DefaultHasher::new();
-            message.data.hash(&mut s);
-            MessageId::from(s.finish().to_string())
-        };
-
-        let gossipsub_config = gossipsub::GossipsubConfigBuilder::default()
-            .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-            .validation_mode(ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
-            .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
-            .build()
-            .expect("Valid config");
-
-        // let topic = Topic::new(topic);
-        let gossipsub = Gossipsub::new(MessageAuthenticity::Signed(local_key.clone()), gossipsub_config)
-            .expect("Correct configuration");
-        // gossipsub.subscribe(&topic)?;
-
         let swarm = {
             let mdns = mdns::async_io::Behaviour::new(mdns::Config::default())?;
-            let behaviour = MpcPubsubBahavior { gossipsub, mdns, keep_alive: keep_alive::Behaviour::default() };
+            let behaviour = MpcPubsubBahavior { 
+                floodsub: Floodsub::new(local_peer_id),
+                mdns, 
+                keep_alive: keep_alive::Behaviour::default() 
+            };
             Swarm::with_async_std_executor(transport, behaviour, local_peer_id)
         };
 
@@ -76,22 +53,13 @@ impl MpcPubsub {
         Ok(())
     }
 
-    pub fn listen_topic(&mut self, topic: &str) -> Result<()> {
-        self.node
-            .borrow_mut()
-            .behaviour_mut()
-            .gossipsub
-            .subscribe(&Topic::new(topic))?;
-        Ok(())
-    }
-
     pub fn process (&mut self, topic: &str) -> Result<(
         impl Stream<Item = Option<Vec<u8>> > + '_, //incoming
         impl Sink<Vec<u8>, Error = anyhow::Error> + '_, // outgoing
     )> {
 
         let mut original_stream = self.node.borrow_mut();
-        original_stream.behaviour_mut().gossipsub.subscribe(&Topic::new(topic))?;
+        original_stream.behaviour_mut().floodsub.subscribe(floodsub::Topic::new(topic));
 
         let notifier = &self.notifier;
 
@@ -114,7 +82,7 @@ impl MpcPubsub {
                             SwarmEvent::Behaviour(MpcPubsubBahaviorEvent::Mdns(mdns::Event::Discovered(list))) => {
                                 for (peer_id, _multiaddr) in list {
                                     println!("mDNS discovered a new peer: {peer_id}");
-                                    original_stream.behaviour_mut().gossipsub.add_explicit_peer(&peer_id);
+                                    original_stream.behaviour_mut().floodsub.add_node_to_partial_view(peer_id);
                                 }
                                 
                                 yield None
@@ -123,17 +91,12 @@ impl MpcPubsub {
                                 println!("Left {:?}", list);
                                 for (peer_id, _multiaddr) in list {
                                     println!("mDNS discover peer has expired: {peer_id}");
-                                    original_stream.behaviour_mut().gossipsub.remove_explicit_peer(&peer_id);
+                                    original_stream.behaviour_mut().floodsub.remove_node_from_partial_view(&peer_id);
                                 }
         
                                 yield None
                             },
-                            SwarmEvent::Behaviour(MpcPubsubBahaviorEvent::Gossipsub(GossipsubEvent::Message {
-                                propagation_source: peer_id,
-                                message_id: id,
-                                message,
-                            })) => {
-                                eprintln!("Got message From {:?}, with ID {:?}", peer_id, id);
+                            SwarmEvent::Behaviour(MpcPubsubBahaviorEvent::Floodsub(floodsub::FloodsubEvent::Message(message))) => {
                                 yield Some(message.data.clone())
                             },
                             _ => {
@@ -146,8 +109,8 @@ impl MpcPubsub {
                         for msg in q.borrow().iter() {
                             let res = original_stream
                                 .behaviour_mut()
-                                .gossipsub
-                                .publish(Topic::new("test"), msg.to_owned());
+                                .floodsub
+                                .publish_any(floodsub::Topic::new("test"), msg.to_owned());
                             eprintln!("Publishing Result: {:?}", res);
                         }
 
